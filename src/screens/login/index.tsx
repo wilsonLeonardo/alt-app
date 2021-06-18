9669638324f7ff6232dafbebc72b5bd3929c720c
Wilson <wilson.chaves@fcamara.com.br>
2021-06-18 12:09:11 -0300

    feat: login screen done

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,25 +1,52 @@
 import React, { useState, useEffect } from 'react';
 import { styles } from './styles'
 //Acesse a documentação do native base para maiores detalhes
-import { Body, Container, Content, Footer, Header, Button } from 'native-base'
-import { Text } from 'react-native';
+import { Body, Container, Content, Footer, Button } from 'native-base'
+import { Text, View, TextInput, Image, StatusBar, TouchableOpacity } from 'react-native';
 import { useNavigation } from '@react-navigation/native'
 
 export default function Login(){
   const { navigate } = useNavigation()
   return (
     <Container style={styles.container}>
-      <Header>
-        <Body>
-          <Text>Alt Tech App</Text>
-        </Body>
-      </Header>
       <Content contentContainerStyle={styles.content}>
-        <Text>Aqui vai o conteudo da tela, botoes, inputs e etc</Text>
-        <Button onPress={() => navigate('Home')}/>
+        <StatusBar barStyle='dark-content'/>
+        <Image source={require('../../../assets/image.png')} style={styles.img}/>
+        <View style={styles.viewInput}>
+          <View style={styles.inputView}>
+            <Text>Email</Text>
+            <TextInput 
+              autoCapitalize='none'
+              style={styles.textInput}/>
+          </View>
+          <View style={styles.inputView}>
+            <Text>Senha</Text>
+            <TextInput 
+              secureTextEntry={true}
+              style={styles.textInput}/>
+          </View>
+        </View>
+        <View style={styles.forgetPassword}>
+          <TouchableOpacity>
+            <Text 
+              style={{color:'#999999', textDecorationLine:'underline'}}>
+                Esqueceu sua senha?
+            </Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.viewButton}>
+          <Button style={styles.button}>
+            <Text style={styles.buttonText}> Login </Text>
+          </Button>
+          <Button style={[styles.button, {backgroundColor:'#4267B2'}]}>
+            <Text style={styles.buttonText}> Continuar com Facebook </Text>
+          </Button>
+        </View>
       </Content>
-      <Footer style={{alignItems: 'center'}}>
-          <Text>Aqui vai a footer</Text>
+      <Footer style={styles.footer}>
+        <TouchableOpacity>
+          <Text style={{color:'#A04FE5', fontSize:18}}>Novo por aqui? Cadastre-se</Text>
+        </TouchableOpacity>
       </Footer>
     </Container>
   )
--- src/screens/login/styles.ts
+++ src/screens/login/styles.ts
@@ -10,5 +10,52 @@ export const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  viewInput: {
+    width:'100%', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    height: '20%'
+  },
+  inputView: {
+    alignItems:'flex-start', 
+    width:'80%'
+  },
+  textInput: {
+    backgroundColor:'#e5e4e2',
+    width:'100%', 
+    height:'55%', 
+    top: '5%', 
+    borderRadius: 5
+  },
+  forgetPassword: {
+    width:'80%', 
+    top:15, 
+    alignItems:'flex-start'
+  },
+  footer: {
+    alignItems: 'center',
+    backgroundColor:'white'
+  },
+  img: {
+    bottom: '10%'
+  },
+  viewButton: {
+    top: '5%',
+    width: '80%',
+    alignItems: 'center',
+    justifyContent:'center'
+  },
+  button: {
+    width: '70%',
+    left: '40%',
+    marginVertical: '3%',
+    alignItems:'center',
+    justifyContent: 'center',
+    backgroundColor:'#A04FE5'
+  },
+  buttonText: {
+    color:'white', 
+    fontWeight:'bold'
   }
 })
